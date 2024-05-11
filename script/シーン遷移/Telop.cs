fda8694fc92e0e6a9c5974e6b9e4da21593385d7
oono-lab <145289101+oono-lab@users.noreply.github.com>
2024-05-12 06:48:36 +0900

    エンドロールに使うスクリプト。telopsで最後に表示されるテキストがendpointに到達したら次のシーンへ移行

--- script/シーン遷移/Telop.cs
+++ script/シーン遷移/Telop.cs
@@ -12,8 +12,17 @@ public class Telop : MonoBehaviour
 
     void Update()
     {
+        text_up_process_for();
+    }
+    void text_up_process_for()
+    {
         for (int i = 0; i < telops.Length; i++)
         {
+            text_up_process();
+        }
+    }
+    void text_up_process()
+    {
             if (Input.GetKey(KeyCode.Space)) telops[i].localPosition += (Vector3.up * .9f)*9;
             else telops[i].localPosition += Vector3.up * .9f;
 
@@ -23,6 +32,7 @@ public class Telop : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
                 FadeManager.Instance.LoadScene("title 1", 1.0f);
             }
-        }
     }
+    
+    
 }
